57a48ad3071f551476b11d735cf7f99fe564d5c0
amandeedo <118350387+amandeedo@users.noreply.github.com>
2023-01-30 08:01:18 +0100

    Update app.py

--- streamlit_dashboard/app.py
+++ streamlit_dashboard/app.py
@@ -18,7 +18,7 @@ from PIL import Image
 import plotly.graph_objects as go
 from IPython.display import HTML
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
-import streamlit-folium as sf
+import streamlit_folium as sf
 from sklearn.cluster import KMeans
 
 
